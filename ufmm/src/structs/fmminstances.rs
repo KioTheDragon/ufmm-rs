1c1497970e93ab3d547e006e4b55af267a55c671
KioTheDragon <kiothedragon@ya.ru>
2026-09-25 23:40:22 +0700

    feat: minor changes

--- ufmm/src/structs/fmminstances.rs
+++ ufmm/src/structs/fmminstances.rs
@@ -1,6 +1,17 @@
-pub mod directsum;
-pub trait UserStorage {}
+use super::Float;
+use super::vectors::cartesian3::Cartesian3;
 
-pub trait FMMInstance {
-    fn compute(user_storage: &mut impl UserStorage) {}
+pub mod directsum;
+pub trait UserStorage<F: Float> {
+    fn len(&self) -> usize;
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+    fn read_position(&self, idx: usize) -> Cartesian3<F>;
+    fn read_velocity(&self, idx: usize) -> Cartesian3<F>;
+    fn write_acceleration(&mut self, idx: usize);
+}
+
+pub trait FMMInstance<F: Float> {
+    fn compute(user_storage: &mut impl UserStorage<F>);
 }
--- ufmm/src/structs/fmminstances/directsum.rs
+++ ufmm/src/structs/fmminstances/directsum.rs
@@ -1,1 +1,8 @@
+use super::FMMInstance;
+use super::Float;
+
 pub struct DirectSum {}
+
+impl<F: Float> FMMInstance<F> for DirectSum {
+    fn compute(user_storage: &mut impl super::UserStorage<F>) {}
+}
